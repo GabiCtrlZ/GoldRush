72b7c3302ff13ac7cccee45af93aeb4b7437b40f
Gabi B <Gab3009@Gmail.com>
2019-07-10 15:01:44 +0300

    finished the model

--- dist/tempManager.js
+++ dist/tempManager.js
@@ -7,5 +7,18 @@ class TempManager{
         promise.then((data) =>{
             this.cityData = data
         })
+        .catch(function(err){console.log(err)})
+    }
+    getCityData(cityName){
+        const promise = $.get('city/' + cityName)
+        promise.then(data => {
+            this.cityData.push(data)
+        })
+        .catch(function(err){console.log(err)})
+    }
+    saveCity(cityName){
+        const data = this.cityData.find(x => x.name == cityName)
+        const promise = $.post('city', data)
+        promise.catch(function(err){console.log(err)})
     }
 }
